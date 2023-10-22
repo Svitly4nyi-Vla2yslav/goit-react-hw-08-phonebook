dda50fee81801634a01feb091c4c9553607121af
Svitly4nyi-Vla2yslav <126347711+Svitly4nyi-Vla2yslav@users.noreply.github.com>
2023-10-22 21:40:04 +0000

    Deploying to gh-pages from @ Svitly4nyi-Vla2yslav/goit-react-hw-08-phonebook@a00ef7b8f577b49ee084b74f38249138af036674 🚀

---
+++ static/js/415.30d30b1c.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[415],{415:function(e,t,o){o.r(t);o(791),o(292);var n=o(184);t.default=function(){return(0,n.jsx)("div",{children:(0,n.jsx)("h1",{className:"Phonebook__title",children:"Phonebook"})})}}}]);
+//# sourceMappingURL=415.30d30b1c.chunk.js.map
