94688f2adb0545b3ba7bf312440fa6a967cf2d10
Svitly4nyi-Vla2yslav <126347711+Svitly4nyi-Vla2yslav@users.noreply.github.com>
2023-10-24 00:19:53 +0200

    home

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -3,21 +3,24 @@ import '../ContactList/ContactList.css'
 import { useDispatch } from 'react-redux'
 import { logOut } from 'redux/auth/auth-operations';
 import { useAuth } from 'redux/auth/auth-selectors';
+import { NavLink } from 'react-router-dom';
 
 export const UserMenu = () => {
     const dispatch = useDispatch();
-    const {user} = useAuth()
+    const { user } = useAuth()
 
     return (
         <div className='container-user_name'>
-            <p className='Contact__item'>Welcome, {user.name} </p>
-            <button 
-            type='button' 
-            className='Delete'
-            onClick={() => dispatch(logOut())}
-            >
-                Logout
-            </button>
+            <p className='Contact__item'>Welcome, 👨‍💻 {user.name} </p>
+            <NavLink to='/'>
+                <button
+                    type='button'
+                    className='Delete log'
+                    onClick={() => dispatch(logOut())}
+                >
+                    Logout
+                </button>
+            </NavLink>
         </div>
     )
 }
